00723e34a9a6c33aaafb9f1c49116528cc099269
arizkyrahman <rizkyrahman7202@gmail.com>
2023-05-05 20:06:55 +0700

    add company details

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -28,6 +28,10 @@ const JobDetails = () => {
     job_id: params.id,
   });
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {};
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -47,7 +51,34 @@ const JobDetails = () => {
           ),
           headerTitle: "",
         }}
-      ></Stack.Screen>
+      />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No Data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   );
 };
