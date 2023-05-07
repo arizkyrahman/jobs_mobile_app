1b42f35798c4177fda6a751b2c1e4c41e68336f0
arizkyrahman <rizkyrahman7202@gmail.com>
2023-05-07 12:44:41 +0700

    add like button

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, Image, Linking } from "react-native";
 
-import styles from './footer.style'
+import styles from "./footer.style";
+import { icons } from "../../../constants";
 
 const Footer = () => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.likeBtn}>
+        <Image
+          source={icons.heartOutline}
+          resizeMode="contain"
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
